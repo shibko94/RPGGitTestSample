19b9236615d92d7e2cf5daf9d91cee8412854cd1
Mikhail Shybko <shibko94@gmail.com>
2023-06-24 19:05:56 +0200

    tererer

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world1");
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
-        System.out.println("Hello yyyyyyyyyyyyyyyy");
+        System.out.println("Hello yyyyyyyyyyyyyyyy555");
     }
 }
