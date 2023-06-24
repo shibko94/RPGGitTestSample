92ef7e2d8d986fab97a6ead637a9414128e0368c
Mikhail Shybko <shibko94@gmail.com>
2023-06-24 15:52:03 +0200

    local fix1-4

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,10 @@ public class Main {
         System.out.println("Hello world1");
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
+
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!!");
     }
 }
